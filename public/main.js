d69ed623eb7f1580766a4a9462b89d52ee2709ab
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-10-24 15:10:49 +0100

    zoho chat integrations, email modified and hosting change

---
+++ public/main.js
@@ -0,0 +1,15 @@
+var $zoho = $zoho || {};
+$zoho.salesiq = $zoho.salesiq || {
+  widgetcode:
+    "siqf0b747aaf367bf0fff606452330af21c162613baf8e4b6a174ca905263794e25",
+  values: {},
+  ready: function () {},
+};
+var d = document;
+s = d.createElement("script");
+s.type = "text/javascript";
+s.id = "zsiqscript";
+s.defer = true;
+s.src = "https://salesiq.zohopublic.com/widget";
+t = d.getElementsByTagName("script")[0];
+t.parentNode.insertBefore(s, t);
