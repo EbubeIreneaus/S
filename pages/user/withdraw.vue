7e4f35402e1f038dffa628be1dba80215d652c35
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2023-12-16 17:32:22 +0100

    Withdrawal section modified

--- pages/user/withdraw.vue
+++ pages/user/withdraw.vue
@@ -13,10 +13,10 @@ const form = reactive({
     channel: "BTC",
 });
 
-const validate_withdrawal =()=> {
-    if (form.amount == '' || form.wallet==''  ||form.channel == '' || form.password == '') {
+const validate_withdrawal = () => {
+    if (form.amount == '' || form.wallet == '' || form.channel == '' || form.password == '') {
         alert("please fill in the form completely before initiating transaction")
-       return false
+        return false
     }
     if (account.value.balance < form.amount) {
         alert("insufficient funds on your account balance")
@@ -59,42 +59,43 @@ const initiate = async (e) => {
         <br />
         <div class="h-max py-2 w-full">
             <section class="">
-                <div class="flex items-center justify-center px-4 py-8 mx-auto sm:px-10 sm:py-0">
-                    <div class="w-full rounded-lg md:mt-0 sm:max-w-md xl:p-0 px-4 py-8 shadow-lg shadow-violet-300">
-                        <div class="space-y-4 md:space-y-6 sm:p-8">
-                            <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl">
+                <div class="flex items-center px-4 py-8  sm:py-0">
+                    <div class="w-full rounded-lg md:mt-0 sm:max-w-2xl xl:p-0 px-4 py-8 shadow-sm">
+                        <div class="space-y-4 md:space-y-6 ">
+                            <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl capitalize">
                                 Initiate A Withdrawal
                             </h1>
-                            <form class="space-y-3 md:space-y-4" @submit.prevent="initiate($event)" autocomplete="off">
-                                <div>
+                            <form class="grid d:grid-cols-2 gap-3 text-white font-sans" @submit.prevent="initiate($event)" autocomplete="off">
+                                <div class="">
                                     <label for="amount" class="block mb-1 text-sm font-medium">Amount</label>
-                                    <input type="text" pattern="[0-9]+"  v-model="form.amount"
-                                        class="!appearance-none w-full py-1 bg-slate-700 my-2 px-3 font-mono border-0 outline-none invalid:ring-2 invalid:ring-red-700" />
+                                    <input type="text" pattern="[0-9]+" v-model="form.amount" placeholder="1000"
+                                        class="w-full py-2 bg-slate-100 my-2 px-3 !text-black/60 font-semibold rounded-sm
+                                        font-mono border-0 outline-none invalid:ring-2 invalid:ring-red-700" />
                                 </div>
-                                <div>
+                                <div class="">
                                     <label for="">Channel</label>
                                     <select v-model="form.channel"
-                                        class="appearance-none w-full bg-slate-700 my-2 px-3 font-mono border-0 outline-none">
+                                        class="appearance-none bg-slate-100 w-full py-2 my-2 px-3 border-0 outline-none !text-black/60 font-semibold">
                                         <option value="BTC">BTC</option>
                                         <option value="USDT">USDT</option>
                                     </select>
                                 </div>
-                                <div>
-                                    <label for="address" class="block mb-1 text-sm font-medium">Wallet</label>
-                                    <input type="text" placeholder="wallet address" v-model="form.wallet"
-                                        class="!appearance-none w-full py-1 bg-slate-700 my-2 px-3 font-mono border-0 
-                                        outline-none invalid:ring-2 invalid:ring-red-700 autofill:!bg-slate-700" />
+                                <div class="w-full md:col-span-2">
+                                    <label for="address" class="block mb-1 text-sm font-medium">Wallet Address</label>
+                                    <textarea placeholder="payment address" class="bg-slate-100  w-full py-2  my-2 px-3 !text-black/60 font-semibold border-0 
+                                        outline-none invalid:ring-2 invalid:ring-red-700" v-model="form.wallet">
+                                        </textarea>
                                 </div>
-                                <div>
+                                <div class="w-full">
                                     <label for="password" class="block mb-1 text-sm font-medium">Password</label>
-                                    <input type="password"  v-model="form.password" 
-                                        class="autofill:!bg-slate-700 w-full py-1 !bg-slate-700 my-2 px-3 font-mono border-0 outline-none invalid:ring-2 invalid:ring-red-700" />
+                                    <input type="password" v-model="form.password" placeholder="password"
+                                        class="w-full bg-slate-100  py-2 my-2 px-3 !text-black/60 font-semibold border-0 outline-none invalid:ring-2 invalid:ring-red-700" />
                                 </div>
-                                <div class="mt-5 text-center">
+                                <div class=" text-center md:col-span-2">
                                     <button type="submit"
-                                        class="ring w-full py-2 mt-3 hover:ring-primary-hover relative group disabled:cursor-not-allowed disabled:ring-0 disabled:border"
+                                        class="ring w-fit px-10 py-3 my-6 hover:ring-primary-hover relative group disabled:cursor-not-allowed disabled:ring-0 disabled:border"
                                         id="sbutton">
-                                        <i class="fas fa-spinner invisible group-disabled:visible animate-spin"></i>
+                                        <i class="fas fa-spinner !hidden group-disabled:!block animate-spin"></i>
                                         Initiate This Withdrawal
                                     </button>
                                 </div>
